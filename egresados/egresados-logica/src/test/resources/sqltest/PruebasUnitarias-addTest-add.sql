befab2ef4118aaac0f43d3189cb7de1d9018c068
Didier_Narváez <Didier_Narvez@Didier-Narvez>
2016-10-19 20:32:23 -0500

    Quinto comit

---
+++ egresados/egresados-logica/src/test/resources/sqltest/PruebasUnitarias-addTest-add.sql
@@ -0,0 +1,2 @@
+insert into db_proyectofinalpruebas.t_facultad(ID_FACULTAD,NOMBRE_FACULTAD)values(1,'ingenieria')
+insert into db_proyectofinalpruebas.t_programa(ID_PROGRAMA,NOMBRE_PROGRAMA,ID_FACULTAD)values(12,'ingenieria de software',1)
---
+++ egresados/egresados-logica/src/test/resources/sqltest/PruebasUnitariasTest-del.sql
@@ -0,0 +1,3 @@
+DELETE FROM `db_proyectofinalpruebas`.`t_egresado` WHERE `ID_EGRESADO`='1';
+DELETE FROM `db_proyectofinalpruebas`.`t_programa` WHERE `ID_PROGRAMA`='12';
+DELETE FROM `db_proyectofinalpruebas`.`t_facultad` WHERE `ID_FACULTAD`='1';
